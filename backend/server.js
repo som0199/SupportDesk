2e873fc26b2b529368ebdda0458aaa9a0e84eee9
som0199 <somyagurjar786@gmail.com>
2024-05-28 16:56:06 +0530

    update cors

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 const cors = require("cors");
-cors();
+app.use(cors());
 //DB connect
 connectDB();
 
